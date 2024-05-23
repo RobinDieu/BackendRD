64a6c67e95ea7c6e19ec17088a7bb69affb4e474
Dieu Robin <robin.dieu@student.howest.be>
2024-05-23 20:44:33 +0200

    Fixed small typo in generate-certs.js

--- generate-certs.js
+++ generate-certs.js
@@ -3,16 +3,16 @@ const path = require("path");
 const selfsigned = require("selfsigned");
 
 // Generate self-signed certificates
-const certs = selfsigned.generate(null, { days: 365 });
-const certsDir = path.join(__dirname, "certs");
+const cert = selfsigned.generate(null, { days: 365 });
+const certDir = path.join(__dirname, "cert");
 
 // Create certs directory if it doesn't exist
-if (!fs.existsSync(certsDir)) {
-  fs.mkdirSync(certsDir, { recursive: true });
+if (!fs.existsSync(certDir)) {
+  fs.mkdirSync(certDir, { recursive: true });
 }
 
 // Write the certificates to files
-fs.writeFileSync(path.join(certsDir, "cert.pem"), certs.cert);
-fs.writeFileSync(path.join(certsDir, "key.pem"), certs.private);
+fs.writeFileSync(path.join(certDir, "cert.pem"), certs.cert);
+fs.writeFileSync(path.join(certDir, "key.pem"), certs.private);
 
 console.log("SSL certificates generated successfully in the certs directory.");
